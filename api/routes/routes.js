d690dc08092d93dfa6498a7d6aa474e8f086e274
nagajyothi.chalamalasetti <nagajyothi.chalamalasetti@motivitylabs.com>
2022-07-14 19:24:04 +0530

    cors issue fixed using node

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,11 +2,13 @@ const express = require('express')
 var authenticationRoutes = require('./authentication.routes')
 var userRoutes = require('./users.routes')
 var visitRoutes=require('./trax.routes')
+var cors=require('cors')
 
 
 
 module.exports = function (app) {
     app.use(express.json())
+    app.use(cors())
     app.use('/', authenticationRoutes)
     app.use('/', userRoutes)
     app.use('/',visitRoutes)
