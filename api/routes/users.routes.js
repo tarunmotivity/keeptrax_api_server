183eb66bfccc396d0f81bf73a259c021c01aa4d2
nagajyothi.chalamalasetti <nagajyothi.chalamalasetti@motivitylabs.com>
2022-07-19 18:12:48 +0530

    update and delete functionalities are added

--- api/routes/users.routes.js
+++ api/routes/users.routes.js
@@ -4,7 +4,7 @@ var router = express.Router()
 var userController = require('../controllers/users.controller')
 
 router.route('/getAllUsers').get(userController.getAllUsers);
-
-
+router.route('/user').put(userController.updateUser)
+router.route('/user').delete(userController.deleteUser)
 
 module.exports = router
--- api/services/users.service.js
+++ api/services/users.service.js
@@ -1,7 +1,15 @@
-var User = require('../models/userModel')
-var dbObj = require('../core/databaseFunction')
-var config = require('../../config/config')
-var logger = config.getLogger(__filename)
+var nodemailer = require('nodemailer');
+var ejs = require('ejs');
+var path = require('path');
+var _ = require('lodash');
+var User = require('../models/userModel');
+var Visits = require('../models/visitsModel');
+var UserPlaces = require('../models/userPlacesModel');
+var dbObj = require('../core/databaseFunction');
+var config = require('../../config/config');
+var TraxService = require("./trax.service");
+var logger = config.getLogger(__filename);
+var CWD = process.cwd();
 
 function getAllUsers(headers, cb) {
     if (headers.managerid && headers.organization && headers.managerid !== null) {
@@ -18,9 +26,65 @@ function getAllUsers(headers, cb) {
             }
         })
     } else {
-        cb({ code: 404, error:"managerid and organizationid are required" })
+        cb({ code: 404, error: "managerid and organizationid are required" })
     }
 
 }
 
-module.exports.getAllUsers = getAllUsers
+function updateUser(headers, userObj, cb) {
+    dbObj.getById(User, { email: headers.email }, function (error, userResp) {
+        if (error) {
+            cb(error)
+        } else {
+            if (headers.managerid == userResp.manager_id) {
+                var updateObj = {
+                    firstname: userObj.firstName,
+                    lastname: userObj.lastName,
+                    lastUpdatedOn: new Date(),
+                    gender: userObj.gender,
+                    birthDate: userObj.birthDate,
+                }
+                dbObj.update(User, updateObj, { email: headers.email }, function (err, response) {
+                    if (err) {
+                        cb(err)
+                    } else {
+                        cb(null, response)
+                    }
+                })
+            } else {
+                cb({ status: 400, message: "You are not eligible to update this user" })
+            }
+        }
+    })
+
+
+}
+
+function deleteUser(headers, cb) {
+    dbObj.getById(User, { email: headers.email }, function (error, userResp) {
+        if (error) {
+            cb(error)
+        } else {
+            if (headers.managerid == userResp.manager_id) {
+                var updateObj = {
+                    manager_id: null
+                }
+                dbObj.update(User, updateObj, { email: headers.email }, function (err, response) {
+                    if (err) {
+                        cb(err)
+                    } else {
+                        cb(null, {status:200 ,message :"User deleted susccessfully"})
+                    }
+                })
+            } else {
+                cb({ status: 400, message: "You are not eligible to delete this user" })
+            }
+        }
+    })
+
+
+}
+
+module.exports.getAllUsers = getAllUsers;
+module.exports.updateUser = updateUser;
+module.exports.deleteUser = deleteUser;
